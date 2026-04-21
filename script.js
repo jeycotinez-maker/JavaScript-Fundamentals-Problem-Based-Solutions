4231d7c241c4e507738b40e43df17f11916acb33
jeyann <jeycotinez@gmail.com>
2026-04-21 16:16:52 +0800

    Amother improvements to my project.

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const card = document.getElementById("card");
 const capacityInput = document.getElementById("capacityInput");
+const applyCapacityBtn = document.getElementById("applyCapacityBtn");
+const entryBtn = document.getElementById("entryBtn");
+const exitBtn = document.getElementById("exitBtn");
+const resetBtn = document.getElementById("resetBtn");
